47237f211e2e373517fda0a6a1455ce4f26702ff
pranaya <pranayatomar@gmail.com>
2018-09-30 23:51:39 +0530

    server serves production bundle now

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 const express = require('express')
-const app = express()
+const path = require('path') 
 const getTshirts = require('./api/controllers/tshirt.controller').getTshirts
 
+const app = express()
+app.use(express.static(path.join(__dirname,'../dist/tshirts')))  
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   next()
 })
 app.get('/tshirts',getTshirts)
+app.use(redirectUnmatched)                                                                                                                                            
+function redirectUnmatched(req,res){                                                                                                                                  
+  res.redirect('/')                                                                                                                                                 
+}     
 const server = app.listen('4000', () => {
   console.log('Listening on 4000')
 })  
