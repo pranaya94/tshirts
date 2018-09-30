469c074817a40fc8d597208d098e4937cf309cf6
pranaya <pranayatomar@gmail.com>
2018-09-30 18:23:25 +0530

    button text changing

--- src/app/components/tshirt-details/tshirt-details.component.ts
+++ src/app/components/tshirt-details/tshirt-details.component.ts
@@ -6,6 +6,7 @@ import { IAppState } from '../../store'
 import { Tshirt } from '../../models/tshirt'
 import { Cart } from '../../models/cart'
 import { SELECT_TSHIRT, ADD_ITEM_TO_CART } from '../../actions/types'
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-tshirt-details',
@@ -20,24 +21,34 @@ export class TshirtDetailsComponent implements OnInit {
   @select() cart$: Observable<Cart[]>
   cart: Cart[]
   tshirtKeys: String[]
+  buttonText: String = 'Add To Cart'
+
+  getButtonText(cart,selectedTshirt){
+    let index = cart.findIndex(item => item.id === selectedTshirt.id)
+      if(index > -1 && cart[index].buyQuantity > 0){
+        return('Add One More')
+      } else {
+        return('Add To Cart')
+      }
+  }
+
+  constructor(private ngRedux: NgRedux<IAppState>, private route: ActivatedRoute){ }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
         this.id = params["id"]
     })
     this.selectedTshirt$.subscribe(selectedTshirt => this.selectedTshirt = selectedTshirt)
+    this.cart$.subscribe(cart => this.cart = cart)
 
     this.ngRedux.dispatch({type: SELECT_TSHIRT, id: this.id})
+    this.buttonText = this.getButtonText(this.cart, this.selectedTshirt)
     this.tshirtKeys = Object.keys(this.selectedTshirt).filter(key => key !== 'picture')
-  }
-  constructor(private ngRedux: NgRedux<IAppState>, private route: ActivatedRoute){
-    this.cart$.subscribe(cart => this.cart = cart)
-    console.log(this.cart)
   }
 
   handleBuyClick(){
     this.ngRedux.dispatch({ type: ADD_ITEM_TO_CART })
-    console.log(this.cart)
+    this.buttonText = this.getButtonText(this.cart, this.selectedTshirt)
   }
 
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MatCardModule, MatButtonModule, MatSelectModule, MatListModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatSelectModule, MatListModule, MatMenuModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [MatCardModule, MatButtonModule, MatSelectModule, BrowserAnimationsModule, MatListModule],
-  exports: [MatCardModule, MatButtonModule, MatSelectModule, BrowserAnimationsModule, MatListModule],
+  imports: [MatCardModule, MatButtonModule, MatSelectModule, BrowserAnimationsModule, MatListModule, MatMenuModule],
+  exports: [MatCardModule, MatButtonModule, MatSelectModule, BrowserAnimationsModule, MatListModule, MatMenuModule],
 })
 export class MaterialModule { }
